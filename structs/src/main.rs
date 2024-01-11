05b63f30d19131f76db5f22dbafb8bbbdfa82bea
zoclhas <zoclhas@gmail.com>
2024-01-11 23:54:31 +0400

    Associated functions

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -1,3 +1,4 @@
+#[derive(Debug)]
 struct Rectanlge {
     width: u32,
     height: u32,
@@ -13,6 +14,15 @@ impl Rectanlge {
     }
 }
 
+impl Rectanlge {
+    fn square(size: u32) -> Rectanlge {
+        Rectanlge {
+            width: size,
+            height: size,
+        }
+    }
+}
+
 fn main() {
     let rect = Rectanlge {
         width: 250,
@@ -22,9 +32,11 @@ fn main() {
         width: 25,
         height: 12,
     };
+    let rect2 = Rectanlge::square(32);
 
     println!("Can rect1 hold rect? {}", rect1.can_hold(&rect));
     println!("Can rect hold rect1? {}", rect.can_hold(&rect1));
     println!("The area of rectangle is {} square units.", rect.area());
+    println!("Square of 32 units: {:#?}", rect2)
 }
 
