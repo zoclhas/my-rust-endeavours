f55ad2b5bfd17d16442953f699212e6a03184223
zoclhas <zoclhas@gmail.com>
2024-01-12 00:11:32 +0400

    Option enum

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -1,28 +1,7 @@
-enum IpAddrKind {
-    V4(u8, u8, u8, u8),
-    V6(),
-}
-
-enum Message {
-    Quit,
-    Move { x: i32, y: i32 },
-    Write(String),
-    ChangeColor(i32, i32, i32),
-}
-
-impl Message {
-    fn some_func() {
-        println!("https://zoch.dev")
-    }
-}
-
-struct IpAddr {
-    kind: IpAddrKind,
-    address: String,
-}
-
 fn main() {
-    let localhost = IpAddrKind::V4(127, 0, 0, 1);
-}
+    let x: i8 = 5;
+    let y: Option<i8> = Some(5);
 
-fn route(ip_kind: IpAddrKind) {}
+    let sum = x + y.unwrap_or(0); // Try to get the value from y, or else use the default value specified in the function
+    println!("{}", sum);
+}
