fad6694711551904e0ef03a7c3bc14fc29d94144
zoclhas <zoclhas@gmail.com>
2024-01-11 23:38:28 +0400

    Tuple structs

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -6,25 +6,8 @@ struct User {
 }
 
 fn main() {
-    let mut user1 = User {
-        username: String::from("Zoclhas"),
-        email: String::from("hi@zoclhas.com"),
-        sign_in_count: 1,
-        active: true,
-    };
-
-    let name = user1.username;
-    user1.username = String::from("Zoch");
-
-    let user2 = build_user(
-        String::from("LokiPoki"),
-        String::from("lokiiscool@loki.dev"),
-    );
-    let user3 = User {
-        username: String::from("John"),
-        email: String::from("john@john.doe"),
-        ..user2
-    };
+    struct Color(i32, i32, i32);
+    struct Point(i32, i32, i32);
 }
 
 fn build_user(email: String, username: String) -> User {
