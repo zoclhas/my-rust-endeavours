f6d351e24a8d75376a39385c06a7ee0430e76859
zoclhas <zoclhas@gmail.com>
2024-01-12 00:05:34 +0400

    Enum metods

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -3,6 +3,19 @@ enum IpAddrKind {
     V6(),
 }
 
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+impl Message {
+    fn some_func() {
+        println!("https://zoch.dev")
+    }
+}
+
 struct IpAddr {
     kind: IpAddrKind,
     address: String,
