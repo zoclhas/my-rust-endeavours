e1b75b834285a8458eeca7173a4435796b30e278
zoclhas <zoclhas@gmail.com>
2024-01-12 14:44:25 +0400

    if let syntax

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -1,14 +1,8 @@
 fn main() {
-    let five = Some(5);
-    let six = plus_one(five);
-    let none = plus_one(None);
-}
+    let some_value = Some(3);
 
-fn plus_one(x: Option<i32>) -> Option<i32> {
-    match x {
-        // If x matches the code below, execute it
-        Some(i) => Some(i + 1),
-        // If any other pattern, do this
-        _ => None,
+    // If some_value mathces Some(3) then do stuff
+    if let Some(3) = some_value {
+        println!("Three");
     }
 }
